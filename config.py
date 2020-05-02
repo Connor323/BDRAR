a6c49d3c5ac6a0d84640db081448a88af5b97d6a
hao66 <hao66@purdue.edu>
2020-05-02 15:19:37 -0500

    change data paths

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # sbu_training_root = './Datasets/SBU/SBUTrain4KRecoveredSmall'
 # sbu_testing_root = './Datasets/SBU/SBU-Test'
 
-sbu_training_root = '/pub1/hao66/dataset/xView2-shadow'
-sbu_testing_root = '/pub1/hao66/dataset/xView2-shadow-test-small'
+sbu_training_root = '/pub1/hao66/dataset/xview2-building-shadow-train'
+sbu_testing_root = '/pub1/hao66/dataset/xview2-building-shadow-test'
